de289ecc1fa13f1200b7f388d13a9bfd767f5dca
Inayatali Seliya <inayatseliya@gmail.com>
2025-01-06 16:36:45 +0530

    Share Member Doctype add require Detail for Form, such as DOB, Phone, Address and Nominee.

--- glint/share_managment/doctype/share_members/share_members.js
+++ glint/share_managment/doctype/share_members/share_members.js
@@ -1,8 +1,13 @@
 // Copyright (c) 2024, Inayatali Seliya and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Share Members", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Share Members", {
+	refresh(frm) {
+        if (!frm.is_new()) {
+            // Make multiple fields read-only
+            ['member_type', 'main_code', 'sub_code', 'title'].forEach(field => {
+                frm.set_df_property(field, 'read_only', 1);
+            });
+        }
+	},
+});
