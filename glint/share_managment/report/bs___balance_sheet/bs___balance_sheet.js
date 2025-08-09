f865a76c87b6cc4fd98e952ded6797b3954ab815
Inayatali Seliya <inayatseliya@gmail.com>
2025-08-09 17:24:44 +0530

    Workspace Cleared

---
+++ glint/share_managment/report/bs___balance_sheet/bs___balance_sheet.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Inayatali Seliya and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["BS - Balance Sheet"] = {
+	"filters": [
+
+	]
+};
